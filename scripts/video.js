879962b64aa537231c990dce02318d14e7cb433d
Sam McEwan <samlmcewan@gmail.com>
2024-03-02 13:43:49 +0000

    video section

--- scripts/video.js
+++ scripts/video.js
@@ -13,4 +13,11 @@ const openModal = () => {
     iframe.src = iframe.src; // Reloads the iframe content to stop the video
   }
 
-  
+//   Pause the video when modal closed 
+document.addEventListener('DOMContentLoaded', function () {
+    var modal = document.getElementById('videoModal');
+    modal.addEventListener('hidden.bs.modal', function () {
+        var iframe = modal.querySelector('iframe');
+        iframe.src = iframe.src; // Pause the video by resetting the src attribute
+    });
+});
