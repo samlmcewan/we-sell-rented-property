a0b9056e1f48775f8ca8d8246213eab933f2e400
Sam McEwan <samlmcewan@gmail.com>
2024-03-02 11:29:57 +0000

    overflow

--- scripts/circle-img-height.js
+++ scripts/circle-img-height.js
@@ -2,4 +2,5 @@ window.onload = function() {
     var circlesHeight = document.querySelector('.buy-sell .container').offsetHeight; // Get the height of the circles
     var img = document.querySelector('.buy-sell img'); // Get the image element
     img.style.minHeight = circlesHeight + 'px'; // Set the minimum height of the image to the height of the circles
+    img.style.maxHeight = circlesHeight + 'px'; // Set the minimum height of the image to the height of the circles
 };
